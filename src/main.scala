992cc76c2e0aca043a8d163b9861b6274378ea12
kairox <git@oxymoron.link>
2025-03-03 20:31:51 +0900

    revert test setup

--- src/main.scala
+++ src/main.scala
@@ -1,4 +1,17 @@
 import serverless.Lambda
 import status.Usecase
 
-@main def main = status.Usecase.check
+@main def main = sys.env("ENV") match {
+  case "local" => handler()
+  case _ =>
+    serverless.Lambda
+      .Handler[serverless.Lambda.CloudWatchScheduledEventRequest](
+        "handler",
+        (event) => {
+          handler()
+          serverless.Lambda.Response(200, "ok")
+        }
+      )
+}
+
+def handler() = status.Usecase.check
